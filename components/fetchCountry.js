d039fa23589d0f1fb60c48599f641f0970c36b87
oussamaZAAM <zaam.oussama@gmail.com>
2023-05-27 18:46:58 +0100

    Fix US, UK, India flags bug

--- components/fetchCountry.js
+++ components/fetchCountry.js
@@ -1,11 +1,18 @@
 export const fetchCountry = async (country) => {
   try {
-    const response = await fetch(
-      `https://restcountries.com/v2/name/${country}`
-    );
+    var response;
+    if (country === "US") {
+      response = await fetch(`https://restcountries.com/v2/name/usa`);
+    } else if (country === "UK") {
+      response = await fetch(`https://restcountries.com/v2/name/united%20kingdom`);
+    } else if (country === "India") {
+      response = await fetch(`https://restcountries.com/v2/name/Bhāra`);
+    } else {
+      response = await fetch(`https://restcountries.com/v2/name/${country}`);
+    }
     if (response.ok) {
       const body = await response.json();
-      return (body[0].flags.png);
+      return body[0].flags.png;
     } else {
       console.log("Error fetching flag");
     }
